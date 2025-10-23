17b5b087f86820a0223f7a99dc4208e260e79201
Aldo Trucios <josealdotrucios@gmail.com>
2025-10-23 12:08:57 -0500

    tf

--- eks-public/addons.tf
+++ eks-public/addons.tf
@@ -29,10 +29,3 @@ resource "aws_eks_addon" "pod_identity_agent" {
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
-resource "aws_eks_addon" "pod_identity_agent" {
-  cluster_name      = aws_eks_cluster.eks_demo.name
-  addon_name        = "eks-pod-identity-agent"
-  addon_version     = "v1.3.8-eksbuild.2"
-  resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
-}
