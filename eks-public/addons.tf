95a2a2164ce11724f8e9b5f132e1c0105f9d4bc1
Aldo Trucios <josealdotrucios@gmail.com>
2025-10-23 12:07:55 -0500

    addons

--- eks-public/addons.tf
+++ eks-public/addons.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_addon" "coredns" {
   cluster_name      = aws_eks_cluster.eks_demo.name
   addon_name        = "coredns"
-  addon_version     = "v1.12.2-eksbuild.4"
+  addon_version     = "v1.12.4-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
@@ -9,7 +9,7 @@ resource "aws_eks_addon" "coredns" {
 resource "aws_eks_addon" "vpc_cni" {
   cluster_name      = aws_eks_cluster.eks_demo.name
   addon_name        = "vpc-cni"
-  addon_version     = "v1.20.1-eksbuild.1"
+  addon_version     = "v1.20.4-eksbuild.1"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
@@ -17,11 +17,18 @@ resource "aws_eks_addon" "vpc_cni" {
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = aws_eks_cluster.eks_demo.name
   addon_name        = "kube-proxy"
-  addon_version     = "v1.33.0-eksbuild.2"
+  addon_version     = "v1.34.0-eksbuild.4"
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
 
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name      = aws_eks_cluster.eks_demo.name
+  addon_name        = "eks-pod-identity-agent"
+  addon_version     = "v1.3.9-eksbuild.3"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
 resource "aws_eks_addon" "pod_identity_agent" {
   cluster_name      = aws_eks_cluster.eks_demo.name
   addon_name        = "eks-pod-identity-agent"
